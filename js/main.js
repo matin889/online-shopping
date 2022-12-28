d087a9db747b404c5cbfbf5a3607d1744c67afa2
Md Abdul Matin <matinpcc@yahoo.com>
2022-12-28 23:09:17 +0100

    products data retrived from JSON file

--- js/main.js
+++ js/main.js
@@ -16,27 +16,28 @@ let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 const generateShop = () => {
     // Generera alla produkter med dynamisk HTML och Array.protype.map() samt join()
-    //
     // Använd denna markup för varje produktkort - den korresponderar mot CSS:en
     //
-    // <div id=product-id-{--id--} class="item">
-    //     <img width="220" src={--url--} alt=""> 
-    //     <div class="details">
-    //         <h3>{--title-}</h3>
-    //         <p>{--desription--}</p>
-    //         <div class="price-quantity">
-    //         <h2>{--price-)-</h2>
-    //         <div class="buttons">
-    //             <i onclick="decrement({--id--})" class="bi bi-dash-lg"></i>
-    //             <div id={--id--} class="quantity">
-    //             </div>
-    //             <div id={--id--} class="quantity">???</div>
-    //             <i onclick="increment({--id--})" class="bi bi-plus-lg"></i>
-    //         </div>
-    //         </div>
-    //     </div>
-    // </div>
-
+    return (shop.innerHTML = shopData.map((item) => {
+        
+        return `<div id=product-id-${item.id} class="item">
+        <img width="220" src=${item.image} alt=""> 
+        <div class="details">
+            <h3>${item.title}</h3>
+            <p>${item.description}</p>
+            <div class="price-quantity">
+            <h2>$ ${item.price}</h2>
+            <div class="buttons">
+                <i onclick="decrement(${item.id})" class="bi bi-dash-lg"></i>
+                <div id=${item.id} class="quantity">
+                </div>
+                <div id=${item.id} class="quantity">???</div>
+                <i onclick="increment(${item.id})" class="bi bi-plus-lg"></i>
+            </div>
+            </div>
+        </div>
+    </div>`
+    }).join(' ')); 
 }
 
 generateShop()
